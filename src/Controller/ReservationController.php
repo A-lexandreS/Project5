555c33113f52bd6edc5e9ff86234e546522eeaf7
Alex C <alexandre.coulombier@outlook.fr>
2022-02-28 12:17:38 +0100

    modif controller and templates architecture

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -6,16 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PostController extends AbstractController
+class ReservationController extends AbstractController
 {
     #[Route('/reservation', name: 'reservation')]
     public function post(): Response
     {
-        return $this->render('post/reservation.html.twig', []);
+        return $this->render('reservation/reservation.html.twig', []);
     }
-    #[Route('/gallery', name: 'gallery')]
-    public function posts(): Response
-    {
-        return $this->render('post/gallery.html.twig', []);
-    }
+
 }
