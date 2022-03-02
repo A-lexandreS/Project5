b908aeff55abdc5757b86e6d91a67d5a4b42a729
Alex C <alexandre.coulombier@outlook.fr>
2022-03-02 18:02:57 +0100

    add Entity Event and User + repository

---
+++ src/Entity/Event.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $picture;
+
+    #[ORM\Column(type: 'datetime')]
+    private $eventDate;
+
+    #[ORM\Column(type: 'datetime')]
+    private $startedAt;
+
+    #[ORM\Column(type: 'datetime')]
+    private $endedAt;
+
+    #[ORM\Column(type: 'integer')]
+    private $maxRegistration;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getEventDate(): ?\DateTimeInterface
+    {
+        return $this->eventDate;
+    }
+
+    public function setEventDate(\DateTimeInterface $eventDate): self
+    {
+        $this->eventDate = $eventDate;
+
+        return $this;
+    }
+
+    public function getStartedAt(): ?\DateTimeInterface
+    {
+        return $this->startedAt;
+    }
+
+    public function setStartedAt(\DateTimeInterface $startedAt): self
+    {
+        $this->startedAt = $startedAt;
+
+        return $this;
+    }
+
+    public function getEndedAt(): ?\DateTimeInterface
+    {
+        return $this->endedAt;
+    }
+
+    public function setEndedAt(\DateTimeInterface $endedAt): self
+    {
+        $this->endedAt = $endedAt;
+
+        return $this;
+    }
+
+    public function getMaxRegistration(): ?int
+    {
+        return $this->maxRegistration;
+    }
+
+    public function setMaxRegistration(int $maxRegistration): self
+    {
+        $this->maxRegistration = $maxRegistration;
+
+        return $this;
+    }
+}
