a43630dc6e28069892c945546d54d34223afb96b
Alex C <alexandre.coulombier@outlook.fr>
2022-04-29 10:53:34 +0200

    css and html of the login form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\UserType;
+use App\Entity\User;
 
 class UserController extends AbstractController
 {
     #[Route('/login')]
     public function login(): Response
     {
-        return $this->render('user/login.html.twig', []);
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        return $this->renderForm('user/login.html.twig', ['form' => $form]);
     }
 }
